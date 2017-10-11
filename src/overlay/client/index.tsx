42c5d6cd108f805a6af6e39f5f54981bf174b54f
ngergo6 <n.gergo6@outlook.com>
2017-10-11 14:15:18 +0200

    overlay skeleton created

--- src/overlay/client/index.tsx
+++ src/overlay/client/index.tsx
@@ -1,1 +1,23 @@
-console.log("hello");
+import * as React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Main } from "./components/Main";
+import { reducers } from "./reducers";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+document.addEventListener("DOMContentLoaded", bootstrap);
+
+function bootstrap() {
+	const store = createStore(reducers, applyMiddleware(thunk));
+
+	render(
+		<Provider store={store}>
+			<Main />
+		</Provider>,
+		document.getElementById("app")
+	);
+
+    // todo: dispatch init action
+}
