9fbfbc9fca6851cafd010eff771aa58edef12438
ngergo6 <n.gergo6@outlook.com>
2017-10-18 14:41:36 +0200

    wire in and test socket.io

---
+++ src/game/client/socket/index.js
@@ -0,0 +1,6 @@
+import * as io from "socket.io-client";
+
+const socket = io();
+
+//socket.connect();
+//socket.emit("handshake", { hello: true });
--- src/socket/index.js
+++ src/socket/index.js
@@ -6,7 +6,13 @@ const socketio = require("socket.io");
 function bootstrap(server) {
     const io = socketio(server);
 
+    io.on("connection", function(socket) {
+        console.log("a user connected");
 
+        socket.on("handshake", function(data) {
+            console.log("handshake", data);
+        });
+    });
 }
 
 module.exports = {
